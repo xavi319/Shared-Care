933ac51f227a6d3a3deeb89f89994d314a245e05
sammaskey <maskeysam10@gmail.com>
2026-04-05 22:45:19 -0700

    Add outputFileTracingRoot to Next.js configuration

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,8 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  reactStrictMode: true
+  reactStrictMode: true,
+  outputFileTracingRoot: process.cwd()
 };
 
 export default nextConfig;
